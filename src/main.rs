e1ca94083b5e62ac5d6169a4ae4f1bb04c30da31
disamii <samsonmamuye360@gmail.com>
2026-04-30 00:14:40 +0300

    csv file reader

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 // use csv;
-use std::fs::File;
+use std::error::Error;
 use std::{env::args, path::Path};
-use std::{error::Error};
 
 fn read_csv<P: AsRef<Path>>(path: P) -> Result<(), Box<dyn Error>> {
-    let csv_file= File::open(path).expect("un able to open file check the pah");
-    todo!()
+    let mut csv_reader = csv::Reader::from_path(path)?;
+
+    for result in csv_reader.records() {
+        let record = result?;
+        println!("{:?}",record)
+    }
+    Ok(())
 }
 
 fn main() {
@@ -15,7 +19,7 @@ fn main() {
         eprintln!("Usage: {} <file_path>", args[0]);
         return;
     }
-    let path=Path::new(&args[1]);
-    let _= read_csv(path);
+    let path = Path::new(&args[1]);
+    let _ = read_csv(path);
     println!("Hello, world!");
 }
